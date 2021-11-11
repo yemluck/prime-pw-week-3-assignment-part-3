069575e8e8b334becc35704cd879a5a0388e0534
Chris Agbolade <yemluck@gmail.com>
2021-11-11 03:54:11 -0600

    modifications to HTML and CSS

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -10,16 +10,20 @@ console.log(partsNeeded);
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
+let supplyChanges = [3, 5, -6, 0, 7, 11]; //console.log(supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
-
+console.log(supplyChanges[1]);
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+let removedSupplyChanges = supplyChanges.pop();
+console.log(removedSupplyChanges);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push(25); //console.log(supplyChanges);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
